1c1b98196f359eafa8e39208ebc54a3935b25024
Mark Lindan <trevel@gmail.com>
2015-04-12 14:27:35 -0400

    Better error management, plus aborts without session.

--- A2_HRManagement/src/ca/myseneca/controller/CRUDEmployee.java
+++ A2_HRManagement/src/ca/myseneca/controller/CRUDEmployee.java
@@ -47,73 +47,84 @@ public class CRUDEmployee extends HttpServlet {
 	 * 
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Employee emp = new Employee();
-		emp.setFirst_name(request.getParameter("fname"));
-		emp.setLast_name(request.getParameter("lname"));
-		emp.setEmail(request.getParameter("email"));
-		emp.setPhone_number(request.getParameter("phone"));
-		java.util.Date hire = null;
 		try {
-			hire = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("hiredate"));
-		} catch (ParseException e) {
-			// If the date is bad, we abort...  
-			getServletContext().getRequestDispatcher("/EditEmployee.jsp").forward(request, response);
-		}
-		emp.setHire_date(new java.sql.Date(hire.getTime()));
-		emp.setJob_id(request.getParameter("jobid"));
-		emp.setSalary(new BigDecimal(request.getParameter("salary")));
-		if (request.getParameter("commpct") == null || request.getParameter("commpct") == "") {
-			emp.setComm_pct(null);
-		} else {
-			emp.setComm_pct(new BigDecimal(request.getParameter("commpct")));
-		}
-		emp.setManager_id(Integer.parseInt(request.getParameter("mgrid")));
-		emp.setDept_id(Integer.parseInt(request.getParameter("deptid")));
-		
-		int result = 0;
-		
-		// Create button
-		if(request.getParameter("create") != null) {
-			emp.setEmployee_id(0); // need to make sure it's initialized before creating
-			result = DBAccessHelper.addNewEmployee(emp);
-			
-			if (result == 0 ) {
-				request.setAttribute("statusmessage", "Could not create record; check fields");
-				this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
-			} else {
-				emp.setEmployee_id(result); // populate emp with the new id.
-				request.getSession(false).setAttribute("notice", "Record successfully created!");
-				response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/" + emp.getEmployee_id()));
+			if (request.getSession(false) == null || request.getSession(false).getAttribute("employee") == null) {
+				getServletContext().getRequestDispatcher("/").forward(request, response);
 				return;
 			}
-			
-		// Update button
-		} else if(request.getParameter("update") != null) {
-			emp.setEmployee_id(Integer.parseInt(request.getParameter("id")));
-			result = DBAccessHelper.updateEmployee(emp);
-			if (result == 0) {
-				request.setAttribute("statusmessage", "Could not update record; check fields");
-				this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
-			} else {
-				request.getSession(false).setAttribute("notice", "Record successfully updated!");
-				response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/" + emp.getEmployee_id()));
-				return;
+			Employee emp = new Employee();
+			emp.setFirst_name(request.getParameter("fname"));
+			emp.setLast_name(request.getParameter("lname"));
+			emp.setEmail(request.getParameter("email"));
+			emp.setPhone_number(request.getParameter("phone"));
+			java.util.Date hire = null;
+			try {
+				hire = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("hiredate"));
+			} catch (ParseException e) {
+				// If the date is bad, we abort...  
+				getServletContext().getRequestDispatcher("/EditEmployee.jsp").forward(request, response);
 			}
-			
-		// Delete button	
-		} else if(request.getParameter("delete") != null) {
-			result = DBAccessHelper.deleteEmployeeByID(Integer.parseInt(request.getParameter("id"))); 
-			if (result == 0 ) {
-				request.setAttribute("statusmessage", "Could not delete record; check fields");
-				this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+			emp.setHire_date(new java.sql.Date(hire.getTime()));
+			emp.setJob_id(request.getParameter("jobid"));
+			emp.setSalary(new BigDecimal(request.getParameter("salary")));
+			if (request.getParameter("commpct") == null || request.getParameter("commpct") == "") {
+				emp.setComm_pct(null);
 			} else {
-				request.getSession(false).setAttribute("notice", "Record deleted! ");
-				response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/new"));
-				return;
+				emp.setComm_pct(new BigDecimal(request.getParameter("commpct")));
 			}
-		}
-		request.setAttribute("statusmessage", "I have no idea what you were trying to do.");
-		this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
-	}
+			emp.setManager_id(Integer.parseInt(request.getParameter("mgrid")));
+			emp.setDept_id(Integer.parseInt(request.getParameter("deptid")));
 
+			int result = 0;
+
+			// Create button
+			if(request.getParameter("create") != null) {
+				emp.setEmployee_id(0); // need to make sure it's initialized before creating
+				result = DBAccessHelper.addNewEmployee(emp);
+
+				if (result == 0 ) {
+					request.setAttribute("statusmessage", "Could not create record; check fields");
+					this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+					return;
+				} else {
+					emp.setEmployee_id(result); // populate emp with the new id.
+					request.getSession(false).setAttribute("notice", "Record successfully created!");
+					response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/" + emp.getEmployee_id()));
+					return;
+				}
+
+				// Update button
+			} else if(request.getParameter("update") != null) {
+				emp.setEmployee_id(Integer.parseInt(request.getParameter("id")));
+				result = DBAccessHelper.updateEmployee(emp);
+				if (result == 0) {
+					request.setAttribute("statusmessage", "Could not update record; check fields");
+					this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+				} else {
+					request.getSession(false).setAttribute("notice", "Record successfully updated!");
+					response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/" + emp.getEmployee_id()));
+					return;
+				}
+				return;
+				// Delete button	
+			} else if(request.getParameter("delete") != null) {
+				result = DBAccessHelper.deleteEmployeeByID(Integer.parseInt(request.getParameter("id"))); 
+				if (result == 0 ) {
+					request.setAttribute("statusmessage", "Could not delete record; check fields");
+					this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+				} else {
+					request.getSession(false).setAttribute("notice", "Record deleted! ");
+					response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/new"));
+					return;
+				}
+				return;
+			}
+			request.setAttribute("statusmessage", "I have no idea what you were trying to do.");
+			this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+		} catch (Exception ex) {
+			request.setAttribute("statusmessage", "Server error; try again later.");
+			this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+			return;
+		}
+	}
 }
--- A2_HRManagement/src/ca/myseneca/controller/EmployeeServlet.java
+++ A2_HRManagement/src/ca/myseneca/controller/EmployeeServlet.java
@@ -31,37 +31,49 @@ public class EmployeeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 * 
 	 * Takes a URL HRM_EMP/{id} and forwards to EditEmployee. If /{id} is a valid record,
-	 * it loads that record in the form, otherwise it gives a new record interface.  
+	 * it loads that record in the form, otherwise it gives a new record interface.
+	 * 
+	 * @param id (optional, from the URL) 
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// To make more RESTful, grabs the number following the URL
-		String url = null;
-		url = (String) request.getAttribute("javax.servlet.forward.request_uri");
-		url = url == null ? ((HttpServletRequest) request).getRequestURI() : url;
-		url = url.split("/")[url.split("/").length - 1];
-		int id;
-		Employee employee = new Employee();
-		if (url.equals("HRM_EMP")) {
-			//getServletContext().getRequestDispatcher("/HRM_EMP/new").forward(request, response);
-			response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/new"));
-			return;
-		} else {
-			if(!url.equals("new")) {
-				try {
-					id = Integer.parseInt(url);
-					employee = DBAccessHelper.getEmployeeByID(id);
-				} catch (NumberFormatException e) {
-					// if it's not a valid integer, we treat it as a new employee. 
-					// Could alternately forward to a list, or some such. 
+		try {
+			if (request.getSession(false) == null || request.getSession(false).getAttribute("employee") == null) {
+				getServletContext().getRequestDispatcher("/").forward(request, response);
+				return;
+			}
+			// To make more RESTful, grabs the number following the URL
+			String url = null;
+			url = (String) request.getAttribute("javax.servlet.forward.request_uri");
+			url = url == null ? ((HttpServletRequest) request).getRequestURI() : url;
+			url = url.split("/")[url.split("/").length - 1];
+			int id;
+			Employee employee = new Employee();
+			if (url.equals("HRM_EMP")) {
+				//getServletContext().getRequestDispatcher("/HRM_EMP/new").forward(request, response);
+				response.sendRedirect(response.encodeRedirectURL("/A2_HRManagement/HRM_EMP/new"));
+				return;
+			} else {
+				if(!url.equals("new")) {
+					try {
+						id = Integer.parseInt(url);
+						employee = DBAccessHelper.getEmployeeByID(id);
+					} catch (NumberFormatException e) {
+						// if it's not a valid integer, we treat it as a new employee. 
+						// Could alternately forward to a list, or some such. 
+					}
 				}
 			}
+			request.setAttribute("emp", employee);
+
+			// ArrayList<Employee> managers = DBAccessHelper.getAllEmployees();
+			ArrayList<Department> departments = DBAccessHelper.getAllDepartments();
+			request.setAttribute("departments", departments);
+			getServletContext().getRequestDispatcher("/EditEmployee.jsp").forward(request, response);
+		} catch (Exception ex) {
+			request.setAttribute("statusmessage", "Server error; try again later.");
+			this.getServletContext().getRequestDispatcher("/statusPage.jsp").forward(request, response);
+			return;
 		}
-		request.setAttribute("emp", employee);
-		
-		// ArrayList<Employee> managers = DBAccessHelper.getAllEmployees();
-		ArrayList<Department> departments = DBAccessHelper.getAllDepartments();
-		request.setAttribute("departments", departments);
-		getServletContext().getRequestDispatcher("/EditEmployee.jsp").forward(request, response);
 	}
 
 	/**
